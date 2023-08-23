cf8c1ac2aae6fe260b1644d358005c49a22dd74e
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-08-23 13:21:43 -0500

    V1

---
+++ 01-starting-setup/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  /* const para = document.createElement('p');
+  para.textContent = 'This is also visible'
+  document.getElementById('root').append(para); */
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>this is also visible!</p>
+    </div>
+  );
+}
+
+export default App;
