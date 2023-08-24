8c760914921af1d8ca350dd0911bdc60b6adeb79
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-08-24 12:41:26 -0500

    V4

--- 01-starting-setup/src/components/Expenseltem.js
+++ 01-starting-setup/src/components/Expenseltem.js
@@ -1,6 +1,12 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+
+    const months = props.date.toLocaleDateString('en-US',{month: 'long'})
+    const day = props.date.toLocaleDateString('en-US',{day: '2-digit'})
+    const year = props.date.getFullYear('en-US',{month: 'long'})
+
+
     return (
         <div className="blog-card spring-fever">
         <div className="title-content">
@@ -15,7 +21,17 @@ function ExpenseItem(props) {
           <ul className="utility-list">
             <li><span className="licon icon-like"></span><a href="#">2</a></li>
             <li><span className="licon icon-com"></span><a href="#">12</a></li>
-            <li><span className="licon icon-dat"></span>{props.date.toISOString()}</li>
+            <li><span className="licon icon-dat a"></span>
+            <div className="a">
+              {months}-
+            </div>
+            <div className="a">
+            {year}-
+            </div>
+            <div className="a">
+            {day}
+            </div>
+            </li>
             <li><span className="licon icon-tag"></span><a href="#">Photos</a>, <a href="#">Nice</a></li>
           </ul>
         </div>
