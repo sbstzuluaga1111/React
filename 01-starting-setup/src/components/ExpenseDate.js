27a433ab9973690fe90b76fb5a95d89107469d79
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2023-08-28 06:57:50 -0500

    V5

---
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,26 @@
+import './ExpenseItem.css'
+import React from "react";
+
+function ExpenseDate(props){
+    const months = props.date.toLocaleDateString('en-US',{month: 'long'})
+    const day = props.date.toLocaleDateString('en-US',{day: '2-digit'})
+    const year = props.date.getFullYear();
+
+    return (    
+        <li><span className="licon icon-dat a"></span>
+        <div className="a">
+        {months}-
+        </div>
+        <div className="a">
+        {year}-
+        </div>
+        <div className="a">
+        {day}
+        </div>
+        </li>
+    );
+
+
+}
+
+export default ExpenseDate;
--- 01-starting-setup/src/components/Expenseltem.js
+++ 01-starting-setup/src/components/Expenseltem.js
@@ -1,13 +1,11 @@
 import './ExpenseItem.css'
+import Card from './Card';
+import React from "react";
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-
-    const months = props.date.toLocaleDateString('en-US',{month: 'long'})
-    const day = props.date.toLocaleDateString('en-US',{day: '2-digit'})
-    const year = props.date.getFullYear('en-US',{month: 'long'})
-
-
     return (
+      <Card>
         <div className="blog-card spring-fever">
         <div className="title-content">
           <h3><a href="#">{props.title}</a></h3>
@@ -21,24 +19,13 @@ function ExpenseItem(props) {
           <ul className="utility-list">
             <li><span className="licon icon-like"></span><a href="#">2</a></li>
             <li><span className="licon icon-com"></span><a href="#">12</a></li>
-            <li><span className="licon icon-dat a"></span>
-            <div className="a">
-              {months}-
-            </div>
-            <div className="a">
-            {year}-
-            </div>
-            <div className="a">
-            {day}
-            </div>
-            </li>
+            <ExpenseDate date={props.date} />
             <li><span className="licon icon-tag"></span><a href="#">Photos</a>, <a href="#">Nice</a></li>
           </ul>
         </div>
         <div className="gradient-overlay"></div>
         <div className="color-overlay"></div>
-      </div>
-     
+      </div></Card>
 );
 }
 
